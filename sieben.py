e7dd848071f2574d4163497be4c2d05945e52ff8
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-11-12 11:27:30 +0500

    Extract class

--- sieben.py
+++ sieben.py
@@ -11,21 +11,29 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import QImage, QPixmap
 
 
+class SiebenApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.scr = QScrollArea()
+        self.l = QLabel()
+        self.img = QImage('./doc/work.png')
+
+    def setup(self):
+        self.setWindowTitle('Sieben 7')
+        self.resize(700, 500)
+        self.scr.setWidget(self.l)
+        self.setCentralWidget(self.scr)
+        self.l.setPixmap(QPixmap.fromImage(self.img))
+        self.l.resize(self.img.size().width(), self.img.size().height())
+        self.l.setScaledContents(True)
+        self.l.setVisible(True)
+        self.scr.setVisible(True)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = QMainWindow()
-    w.setWindowTitle('Sieben 7')
-    w.resize(500, 400)
-    scr = QScrollArea()
-    l = QLabel()
-    scr.setWidget(l)
-    w.setCentralWidget(scr)
-    img = QImage('./doc/work.png')
-    l.setPixmap(QPixmap.fromImage(img))
-    l.resize(img.size().width(), img.size().height())
-    l.setScaledContents(True)
-    l.setVisible(True)
-    scr.setVisible(True)
+    w = SiebenApp()
+    w.setup()
     w.show()
 
     sys.exit(app.exec_())
