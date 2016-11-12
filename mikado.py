53f22ff3c15f78c692358155a74f7e1f443b37c9
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-11-12 12:12:14 +0500

    Next test: addition

--- mikado.py
+++ mikado.py
@@ -5,8 +5,12 @@ class Goals():
     def __init__(self, name):
         self.goals = {1: name}
 
+    def add(self, name):
+        self.goals[2] = name
+
     def all(self):
         return self.goals
 
     def top(self):
-        return self.goals
+        key = sorted(self.goals.keys())[-1]
+        return {key: self.goals[key]}
--- test_mikado.py
+++ test_mikado.py
@@ -4,5 +4,12 @@ from mikado import Goals
 
 def test_there_is_one_goal_at_start():
     goals = Goals('Just a root goal')
-    assert goals.top() == {1: 'Just a root goal'}
     assert goals.all() == {1: 'Just a root goal'}
+    assert goals.top() == {1: 'Just a root goal'}
+
+
+def test_new_goal_moves_to_top():
+    goals = Goals('Just do it')
+    goals.add('Some later')
+    assert goals.all() == {1: 'Just do it', 2: 'Some later'}
+    assert goals.top() == {2: 'Some later'}
