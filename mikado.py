359505fab2acbee806bb24e4ccb1880f903bee2a
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-11-12 12:08:12 +0500

    Define simple API

--- mikado.py
+++ mikado.py
@@ -3,4 +3,10 @@
 
 class Goals():
     def __init__(self, name):
-        pass
+        self.goals = {1: name}
+
+    def all(self):
+        return self.goals
+
+    def top(self):
+        return self.goals
--- test_mikado.py
+++ test_mikado.py
@@ -4,3 +4,5 @@ from mikado import Goals
 
 def test_there_is_one_goal_at_start():
     goals = Goals('Just a root goal')
+    assert goals.top() == {1: 'Just a root goal'}
+    assert goals.all() == {1: 'Just a root goal'}
