9ed9c0dfc13459d146bb2132b4bbcd3f605fac56
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-11-27 12:54:18 +0500

    Improve Hypothesis tests efficiency
    With the previous way of creating lists of actions most of examples
    seemed different to Hypothesis (because they have different integers),
    but not to the build_from (because most of these integers were just
    ignored). The new generator model requires additional parameter, but it
    should generate more diverse lists of examples (because no generated
    data is ignored now).

--- test_mikado_properties.py
+++ test_mikado_properties.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from mikado import Goals
 from hypothesis import given, note, assume, example
-from hypothesis.strategies import integers, lists, tuples, sampled_from, composite, choices
+from hypothesis.strategies import integers, lists, sampled_from, composite, choices, streaming
 
 
 USER_ACTIONS = {
@@ -18,57 +18,48 @@ USER_ACTIONS = {
 
 @composite
 def user_actions(draw, min_size=0, skip=[]):
-    actions = draw(lists(tuples(
-        sampled_from(k for k in USER_ACTIONS.keys() if k not in skip),
-        integers(0, 9)
-    ), min_size=min_size))
-    return actions
+    return draw(lists(sampled_from(k for k in USER_ACTIONS.keys() if k not in skip),
+                      min_size=min_size))
 
 
-def pretty_print(actions):
-    result = []
-    for name, int_val in actions:
-        if name == 'select':
-            result.append('%s %d' % (name, int_val))
-        elif name in ['add', 'insert']:
-            result.append('%s foo' % name)
-        else:
-            result.append(name)
-    return result
-
-
-def build_from(actions):
-    note(pretty_print(actions))
+def build_from(actions, ints):
+    ints_index = 0
     g = Goals('Root')
-    for name, int_val in actions:
+    for name in actions:
+        int_val = 0
+        if name == 'select':
+            int_val = ints[ints_index]
+            ints_index += 1
         USER_ACTIONS[name](g, int_val)
     return g
 
 
-@given(user_actions())
-def test_there_is_always_at_least_one_goal(actions):
-    g = build_from(actions)
+@given(user_actions(), streaming(integers(0, 9)))
+def test_there_is_always_at_least_one_goal(actions, ints):
+    g = build_from(actions, ints)
     assert g.all()
 
 
-@given(user_actions())
-def test_there_is_always_one_selected_goal(actions):
-    g = build_from(actions)
+@given(user_actions(), streaming(integers(0, 9)))
+def test_there_is_always_one_selected_goal(actions, ints):
+    g = build_from(actions, ints)
     assert len([1 for k, v in g.all(keys='select').items() if v]) == 1
 
 
-@given(user_actions(min_size=15,skip=['rename']), user_actions(min_size=1, skip=['select']), choices())
-def test_any_goal_may_be_selected(all_actions, non_select_actions, choice):
-    g = build_from(all_actions + non_select_actions)
+@given(user_actions(min_size=15,skip=['rename']),
+       user_actions(min_size=1, skip=['select']),
+       streaming(integers(0, 9)), choices())
+def test_any_goal_may_be_selected(all_actions, non_select_actions, ints, choice):
+    g = build_from(all_actions + non_select_actions, ints)
     rnd_goal = choice(list(g.all().keys()))
     for i in str(rnd_goal):
         g.select(int(i))
     assert g.all(keys='select')[rnd_goal] == True
 
 
-@given(user_actions(skip=['rename']))
-def test_all_goals_must_be_connected_to_the_root(actions):
-    g = build_from(actions)
+@given(user_actions(skip=['rename']), streaming(integers(0, 9)))
+def test_all_goals_must_be_connected_to_the_root(actions, ints):
+    g = build_from(actions, ints)
     edges = g.all(keys='edge')
     queue, visited = [k for k, v in g.all().items() if v == 'Root'], set()
     while queue:
@@ -78,9 +69,9 @@ def test_all_goals_must_be_connected_to_the_root(actions):
     assert visited == set(edges.keys())
 
 
-@given(user_actions(skip=['rename']))
-def test_all_open_goals_must_be_connected_to_the_root_via_other_open_goals(actions):
-    g = build_from(actions)
+@given(user_actions(skip=['rename']), streaming(integers(0, 9)))
+def test_all_open_goals_must_be_connected_to_the_root_via_other_open_goals(actions, ints):
+    g = build_from(actions, ints)
     open_goals = set(k for k, v in g.all(keys='open').items() if v)
     edges = {k: v for k, v in g.all(keys='edge').items() if k in open_goals}
     assume(edges)
