d78adf97e9c9c9f868b89f4f9c9afd272b8bb225
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-12-06 10:59:45 +0500

    Merge tests on save/load

--- siebenapp/tests/test_database.py
+++ siebenapp/tests/test_database.py
@@ -1,7 +1,8 @@
 # coding: utf-8
+import pickle
 import sqlite3
 from siebenapp.goaltree import Goals
-from siebenapp.system import MIGRATIONS, run_migrations, load
+from siebenapp.system import MIGRATIONS, run_migrations, load, save
 from tempfile import NamedTemporaryFile
 
 
@@ -64,3 +65,46 @@ def test_restore_goals_from_db():
     expected_goals.select(2)
     assert expected_goals.all(keys='name,edge,open,select') == \
             actual_goals.all(keys='name,edge,open,select')
+
+
+def test_load_from_missing_file():
+    file_name = NamedTemporaryFile().name
+    expected_goals = Goals('Rename me')
+    new_goals = load(file_name)
+    assert new_goals.all(keys='open,name,edge,select') == \
+            expected_goals.all(keys='open,name,edge,select')
+
+
+def test_load_from_pickle_dump():
+    file_name = NamedTemporaryFile().name
+    goals = Goals('Root')
+    goals.add('Top')
+    goals.add('Middle')
+    goals.select(3)
+    goals.hold_select()
+    goals.select(2)
+    goals.toggle_link()
+    goals.add('Closed')
+    goals.select(4)
+    goals.toggle_close()
+    with open(file_name, 'wb') as f:
+        pickle.dump(goals, f)
+    new_goals = load(file_name)
+    assert goals.all(keys='open,name,edge,select') == new_goals.all(keys='open,name,edge,select')
+
+
+def test_save_and_load():
+    file_name = NamedTemporaryFile().name
+    goals = Goals('Root')
+    goals.add('Top')
+    goals.add('Middle')
+    goals.select(3)
+    goals.hold_select()
+    goals.select(2)
+    goals.toggle_link()
+    goals.add('Closed')
+    goals.select(4)
+    goals.toggle_close()
+    save(goals, file_name)
+    new_goals = load(file_name)
+    assert goals.all(keys='open,name,edge,select') == new_goals.all(keys='open,name,edge,select')
