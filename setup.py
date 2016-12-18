a225778fc716b811d780939cdfe729a0e87a09cd
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-12-18 13:42:20 +0500

    Start version 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='SiebenApp',
-    version='0.2',
+    version='0.3',
     description='An experimental dependency-aware goal manager',
     author='Andrey Hitrin',
     author_email='andrey.hitrin@gmail.com',
