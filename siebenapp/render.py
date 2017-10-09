e9490ff7c60f448e817510eae8e5d67c00f27324
Andrey Hitrin <andrey.hitrin@gmail.com>
2017-10-09 11:04:16 +0500

    Try simple "longest path" layering algo #5

--- siebenapp/render.py
+++ siebenapp/render.py
@@ -1,5 +1,29 @@
+from collections import defaultdict
+
+
 def render_tree(goals, render_log):
-    data = goals.all(keys='edge')
-    for k in sorted(data.keys()):
-        render_log.append('%s -> %s' % (str(k), str(data[k]['edge'])))
-    render_log.append("Total goals: %d" % len(data))
+    edges = {key: values['edge'] for key, values in goals.all(keys='edge').items()}
+    render_log.append("Total goals: %d" % len(edges))
+    layers = longest_path(edges)
+    for l in sorted(layers.keys()):
+        render_log.append('Layer %s -> %s' % (l, layers[l]))
+
+
+def longest_path(source):
+    layers = defaultdict(list)
+    current_layer = 1
+    layered = set()
+    allowed_targets = set()
+    edges = set(source.keys())
+
+    while layered != edges:
+        candidates = {k for k in edges.difference(layered)
+                      if all(e in allowed_targets for e in source[k])}
+        if candidates:
+            next_candidate = candidates.pop()
+            layers[current_layer].append(next_candidate)
+            layered.add(next_candidate)
+        else:
+            current_layer += 1
+            allowed_targets.update(layered)
+    return dict(layers)
