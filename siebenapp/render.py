3c1c5fe658558c66461961edb3e92b08c768b6c7
Andrey Hitrin <andrey.hitrin@gmail.com>
2018-01-21 18:45:27 +0500

    Move edges into field

--- siebenapp/render.py
+++ siebenapp/render.py
@@ -8,12 +8,13 @@ def render_tree(goals):
 class Renderer:
     def __init__(self, goals):
         self.goals = goals
+        self.edges = {}
 
     def build(self):
         graph = self.goals.all(keys='name,edge,open,select,switchable')
-        edges = {key: values['edge'] for key, values in graph.items()}
-        layers = self.min_width(edges, 4)
-        self.reorder(edges, layers)
+        self.edges = {key: values['edge'] for key, values in graph.items()}
+        layers = self.min_width(4)
+        self.reorder(layers)
         for row in sorted(layers.keys()):
             for col, goal_id in enumerate(layers[row]):
                 if goal_id not in graph:
@@ -27,13 +28,13 @@ class Renderer:
                 graph[goal_id].update({
                     'row': row,
                     'col': col,
-                    'edge': edges[goal_id],
+                    'edge': self.edges[goal_id],
                 })
         return graph
 
     # pylint: disable=too-many-locals
-    def min_width(self, source, width):
-        unsorted_goals, sorted_goals, goals_on_previous_layers = dict(source), set(), set()
+    def min_width(self, width):
+        unsorted_goals, sorted_goals, goals_on_previous_layers = dict(self.edges), set(), set()
         layers = defaultdict(list)
         current_layer, width_current, width_up = 0, 0, 0
         incoming_edges, outgoing_edges = set(), set()
@@ -48,7 +49,7 @@ class Renderer:
                 except KeyError:
                     pass
                 layers[current_layer].append(goal)
-                back_edges = [k for k, vs in source.items() if goal in vs]
+                back_edges = [k for k, vs in self.edges.items() if goal in vs]
                 outgoing_edges.update(e for e in back_edges)
                 width_current += 1 - len(edges)
                 width_up += len(back_edges)
@@ -56,11 +57,11 @@ class Renderer:
                     break
             for original_id in incoming_edges:
                 new_goal_name = '%d_%d' % (original_id, current_layer)
-                source[new_goal_name] = [g for g in source[original_id]
-                                         if g in sorted_goals and g not in layers[current_layer]]
-                for g in source[new_goal_name]:
-                    source[original_id].remove(g)
-                source[original_id].append(new_goal_name)
+                self.edges[new_goal_name] = [g for g in self.edges[original_id]
+                                             if g in sorted_goals and g not in layers[current_layer]]
+                for g in self.edges[new_goal_name]:
+                    self.edges[original_id].remove(g)
+                self.edges[original_id].append(new_goal_name)
                 layers[current_layer].append(new_goal_name)
                 sorted_goals.add(new_goal_name)
             current_layer += 1
@@ -70,7 +71,7 @@ class Renderer:
             outgoing_edges.clear()
         return dict(layers)
 
-    def reorder(self, edges, layers):
+    def reorder(self, layers):
         for curr_layer in sorted(layers.keys(), reverse=True)[:-1]:
             fixed_line = layers[curr_layer]
             fixed_positions = {g: i for i, g in enumerate(fixed_line)}
@@ -78,7 +79,7 @@ class Renderer:
             random_positions = {g: i for i, g in enumerate(random_line)}
             deltas = defaultdict(list)
             for goal in fixed_line:
-                for e in edges[goal]:
+                for e in self.edges[goal]:
                     deltas[e].append(fixed_positions[goal] - random_positions[e])
             gravity = []
             for goal in random_line:
