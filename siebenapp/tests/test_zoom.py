9a60ddeb63333894515513f5f70240a4c33358b8
Andrey Hitrin <andrey.hitrin@gmail.com>
2018-06-04 11:58:49 +0500

    Reorder test cases for zoom #17

--- siebenapp/tests/test_zoom.py
+++ siebenapp/tests/test_zoom.py
@@ -41,25 +41,6 @@ def test_hide_neighbour_goals_during_zoom():
     }
 
 
-def test_double_zoom_means_unzoom():
-    goals = Zoom(build_goaltree(
-        open_(1, 'Root', [2, 3]),
-        open_(2, 'Zoomed', select=selected),
-        open_(3, 'Hidden')
-    ))
-    goals.toggle_zoom()
-    assert goals.q() == {
-        -1: {'name': 'Root'},
-        2: {'name': 'Zoomed'},
-    }
-    goals.toggle_zoom()
-    assert goals.q('name,edge') == {
-        1: {'name': 'Root', 'edge': [2, 3]},
-        2: {'name': 'Zoomed', 'edge': []},
-        3: {'name': 'Hidden', 'edge': []},
-    }
-
-
 def test_do_not_hide_subgoals():
     goals = Zoom(build_goaltree(
         open_(1, 'Root', [2]),
@@ -78,6 +59,25 @@ def test_do_not_hide_subgoals():
         2: {'name': 'Zoomed', 'edge': [3]},
         3: {'name': 'Visible', 'edge': [4]},
         4: {'name': 'More children', 'edge': []},
+    }
+
+
+def test_double_zoom_means_unzoom():
+    goals = Zoom(build_goaltree(
+        open_(1, 'Root', [2, 3]),
+        open_(2, 'Zoomed', select=selected),
+        open_(3, 'Hidden')
+    ))
+    goals.toggle_zoom()
+    assert goals.q() == {
+        -1: {'name': 'Root'},
+        2: {'name': 'Zoomed'},
+    }
+    goals.toggle_zoom()
+    assert goals.q('name,edge') == {
+        1: {'name': 'Root', 'edge': [2, 3]},
+        2: {'name': 'Zoomed', 'edge': []},
+        3: {'name': 'Hidden', 'edge': []},
     }
 
 
@@ -105,18 +105,19 @@ def test_selection_should_be_changed_if_selected_goal_is_invisible():
     assert goals.events[-1] == ('hold_select', 3)
 
 
-def test_zoom_events():
-    goals = Zoom(Goals('Root'))
-    goals.add('Intermediate')
-    goals.add('Zoom here', 2)
+def test_closing_zoom_root_should_cause_unzoom():
+    goals = Zoom(build_goaltree(
+        open_(1, 'Root', [2]),
+        open_(2, 'Intermediate', [3]),
+        open_(3, 'Zoom here', select=selected)
+    ))
     goals.toggle_zoom()
-    assert all(e[0] != 'select' for e in goals.events)
-    goals.select(3)
-    goals.hold_select()
-    goals.toggle_zoom()
-    assert goals.events[-1] == ('zoom', 3)
-    goals.toggle_zoom()
-    assert goals.events[-1] == ('zoom', 1)
+    goals.toggle_close()
+    assert goals.q(keys='name,select,open') == {
+        1: {'name': 'Root', 'select': 'select', 'open': True},
+        2: {'name': 'Intermediate', 'select': None, 'open': True},
+        3: {'name': 'Zoom here', 'select': None, 'open': False},
+    }
 
 
 def test_goal_closing_must_not_cause_root_selection():
@@ -163,6 +164,20 @@ def test_goal_reopening_must_not_change_selection():
     }
 
 
+def test_deleting_zoom_root_should_cause_unzoom():
+    goals = Zoom(build_goaltree(
+        open_(1, 'Root', [2]),
+        open_(2, 'Intermediate', [3]),
+        open_(3, 'Zoom here', select=selected)
+    ))
+    goals.toggle_zoom()
+    goals.delete()
+    assert goals.q(keys='name,select,open') == {
+        1: {'name': 'Root', 'select': 'select', 'open': True},
+        2: {'name': 'Intermediate', 'select': None, 'open': True},
+    }
+
+
 def test_goal_deletion_must_not_cause_root_selection():
     goals = Zoom(build_goaltree(
         open_(1, 'Root', [2]),
@@ -184,30 +199,15 @@ def test_goal_deletion_must_not_cause_root_selection():
     }
 
 
-def test_closing_zoom_root_should_cause_unzoom():
-    goals = Zoom(build_goaltree(
-        open_(1, 'Root', [2]),
-        open_(2, 'Intermediate', [3]),
-        open_(3, 'Zoom here', select=selected)
-    ))
+def test_zoom_events():
+    goals = Zoom(Goals('Root'))
+    goals.add('Intermediate')
+    goals.add('Zoom here', 2)
     goals.toggle_zoom()
-    goals.toggle_close()
-    assert goals.q(keys='name,select,open') == {
-        1: {'name': 'Root', 'select': 'select', 'open': True},
-        2: {'name': 'Intermediate', 'select': None, 'open': True},
-        3: {'name': 'Zoom here', 'select': None, 'open': False},
-    }
-
-
-def test_deleting_zoom_root_should_cause_unzoom():
-    goals = Zoom(build_goaltree(
-        open_(1, 'Root', [2]),
-        open_(2, 'Intermediate', [3]),
-        open_(3, 'Zoom here', select=selected)
-    ))
+    assert all(e[0] != 'select' for e in goals.events)
+    goals.select(3)
+    goals.hold_select()
     goals.toggle_zoom()
-    goals.delete()
-    assert goals.q(keys='name,select,open') == {
-        1: {'name': 'Root', 'select': 'select', 'open': True},
-        2: {'name': 'Intermediate', 'select': None, 'open': True},
-    }
+    assert goals.events[-1] == ('zoom', 3)
+    goals.toggle_zoom()
+    assert goals.events[-1] == ('zoom', 1)
