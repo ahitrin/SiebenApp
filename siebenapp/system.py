9605a5953f544039ea49f511d1d184281b5339d6
Andrey Hitrin <andrey.hitrin@gmail.com>
2018-07-12 17:52:07 +0500

    Fix migration #17

--- siebenapp/system.py
+++ siebenapp/system.py
@@ -70,8 +70,9 @@ MIGRATIONS = [
             goal integer,
             foreign key(goal) references goals(goal_id)
         )''',
-        '''insert into zoom (level, goal) select 1, goal from settings
-           where name = 'zoom' ''',
+        'insert into zoom (level, goal) values (1, 1)',
+        '''insert into zoom (level, goal) select 2, goal from settings
+           where name = 'zoom' and goal <> 1''',
         "delete from settings where name = 'zoom'",
     ],
 ]
