1225235e57800083dd4b9906df756bf9663ee834
Andrey Hitrin <andrey.hitrin@gmail.com>
2018-08-27 09:46:25 +0500

    Type annotations for goaltree.py

--- siebenapp/goaltree.py
+++ siebenapp/goaltree.py
@@ -1,26 +1,30 @@
 # coding: utf-8
 import collections
+from typing import Callable, Dict, Optional, List, Set, Any, Tuple
 
 
 class Goals:
     def __init__(self, name, message_fn=None):
-        self.goals = {}
-        self.edges = {}
-        self.back_edges = {}
-        self.closed = set()
-        self.settings = {
+        # type: (str, Callable[[str], None]) -> None
+        self.goals = {}  # type: Dict[int, Optional[str]]
+        self.edges = {}  # type: Dict[int, List[int]]
+        self.back_edges = {}  # type: Dict[int, List[int]]
+        self.closed = set()  # type: Set[int]
+        self.settings = {  # type: Dict[str, int]
             'selection': 1,
             'previous_selection': 1,
         }
-        self.events = collections.deque()
+        self.events = collections.deque()  # type: collections.deque
         self.message_fn = message_fn
         self._add_no_link(name)
 
     def _msg(self, message):
+        # type: (str) -> None
         if self.message_fn:
             self.message_fn(message)
 
     def add(self, name, add_to=0):
+        # type: (str, int) -> bool
         if add_to == 0:
             add_to = self.settings['selection']
         if add_to in self.closed:
@@ -31,6 +35,7 @@ class Goals:
         return True
 
     def _add_no_link(self, name):
+        # type: (str) -> int
         next_id = max(list(self.goals.keys()) + [0]) + 1
         self.goals[next_id] = name
         self.edges[next_id] = list()
@@ -39,30 +44,36 @@ class Goals:
         return next_id
 
     def select(self, goal_id):
+        # type: (int) -> None
         if goal_id in self.goals and self.goals[goal_id] is not None:
             self.settings['selection'] = goal_id
             self.events.append(('select', goal_id))
 
     def hold_select(self):
+        # type: () -> None
         self.settings['previous_selection'] = self.settings['selection']
         self.events.append(('hold_select', self.settings['selection']))
 
     def q(self, keys='name'):
+        # type: (str) -> Dict[str, Any]
         """Run search query against goaltree state"""
+
         def sel(x):
+            # type: (int) -> Optional[str]
             if x == self.settings['selection']:
                 return 'select'
             elif x == self.settings['previous_selection']:
                 return 'prev'
             return None
+
         keys = keys.split(',')
-        result = dict()
+        result = dict()  # type: Dict[str, Any]
         for key, name in ((k, n) for k, n in self.goals.items() if n is not None):
             switchable = (
-                (key not in self.closed and
-                 all(x in self.closed for x in self.edges[key])) or
-                (key in self.closed and (not self.back_edges[key] or
-                                         any(x for x in self.back_edges[key] if x not in self.closed))))
+                    (key not in self.closed and
+                     all(x in self.closed for x in self.edges[key])) or
+                    (key in self.closed and (not self.back_edges[key] or
+                                             any(x for x in self.back_edges[key] if x not in self.closed))))
             value = {
                 'edge': sorted(self.edges[key]),
                 'name': name,
@@ -74,6 +85,7 @@ class Goals:
         return result
 
     def insert(self, name):
+        # type: (str) -> None
         if self.settings['selection'] == self.settings['previous_selection']:
             self._msg("A new goal can be inserted only between two different goals")
             return
@@ -84,18 +96,21 @@ class Goals:
                 self.toggle_link(self.settings['previous_selection'], self.settings['selection'])
 
     def rename(self, new_name, goal_id=0):
+        # type: (str, int) -> None
         if goal_id == 0:
             goal_id = self.settings['selection']
         self.goals[goal_id] = new_name
         self.events.append(('rename', new_name, goal_id))
 
     def swap_goals(self):
+        # type: () -> None
         first, second = self.settings['selection'], self.settings['previous_selection']
         first_name, second_name = self.goals[first], self.goals[second]
         self.rename(first_name, second)
         self.rename(second_name, first)
 
     def toggle_close(self):
+        # type: () -> None
         if self.settings['selection'] in self.closed:
             if self._may_be_reopened():
                 self.closed.remove(self.settings['selection'])
@@ -112,13 +127,16 @@ class Goals:
                 self._msg("This goal can't be closed because it have open subgoals")
 
     def _may_be_closed(self):
+        # type: () -> bool
         return all(g in self.closed for g in self.edges[self.settings['selection']])
 
     def _may_be_reopened(self):
+        # type: () -> bool
         parent_goals = self.back_edges[self.settings['selection']]
         return all(g not in self.closed for g in parent_goals)
 
     def delete(self, goal_id=0):
+        # type: (int) -> None
         if goal_id == 0:
             goal_id = self.settings['selection']
         if goal_id == 1:
@@ -129,6 +147,7 @@ class Goals:
         self.hold_select()
 
     def _delete(self, goal_id):
+        # type: (int) -> None
         self.goals[goal_id] = None
         self.closed.add(goal_id)
         next_to_remove = self.edges.pop(goal_id, {})
@@ -145,6 +164,7 @@ class Goals:
         self.events.append(('delete', goal_id))
 
     def toggle_link(self, lower=0, upper=0):
+        # type: (int, int) -> None
         lower = self.settings['previous_selection'] if lower == 0 else lower
         upper = self.settings['selection'] if upper == 0 else upper
         if lower == upper:
@@ -156,6 +176,7 @@ class Goals:
             self._create_new_link(lower, upper)
 
     def _remove_existing_link(self, lower, upper):
+        # type: (int, int) -> None
         edges_to_upper = len(self.back_edges[upper])
         if edges_to_upper > 1:
             self.edges[lower].remove(upper)
@@ -165,6 +186,7 @@ class Goals:
             self._msg("Can't remove the last link")
 
     def _create_new_link(self, lower, upper):
+        # type: (int, int) -> None
         if lower in self.closed and upper not in self.closed:
             self._msg("An open goal can't block already closed one")
             return
@@ -184,6 +206,7 @@ class Goals:
             self._msg("Circular dependencies between goals are not allowed")
 
     def verify(self):
+        # type: () -> bool
         assert all(g in self.closed for p in self.closed for g in self.edges.get(p, [])), \
             'Open goals could not be blocked by closed ones'
 
@@ -206,6 +229,7 @@ class Goals:
 
     @staticmethod
     def build(goals, edges, settings, message_fn=None):
+        # type: (List[Tuple[int, str]], List[Tuple[int, int]], List[Tuple[str, int]], Callable[[str], None]) -> Goals
         result = Goals('', message_fn)
         result.events.clear()  # remove initial goal
         goals_dict = dict((g[0], g[1]) for g in goals)
@@ -228,6 +252,7 @@ class Goals:
 
     @staticmethod
     def export(goals):
+        # type: (Goals) -> Tuple[List[Tuple[int, str, bool]], List[Tuple[int, int]], List[Tuple[str, int]]]
         nodes = [(g_id, g_name, g_id not in goals.closed)
                  for g_id, g_name in goals.goals.items()]
         edges = [(parent, child) for parent in goals.edges
