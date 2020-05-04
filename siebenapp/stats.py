4e1083294024b8ff1d8d1b47eb77c3c0e4bed719
Андрей Хитрин <andrey.hitrin@gmail.com>
2020-05-04 14:18:57 +0500

    Fix formatting

--- siebenapp/stats.py
+++ siebenapp/stats.py
@@ -9,14 +9,17 @@ def complexity(goals: Goals) -> Dict[str, int]:
     paths_count = {1: 0}
     to_visit = goals._forward_edges(1)
     while to_visit:
-        free_goals = [g for g in to_visit
-                      if all(p in paths_count for p in goals._back_edges(g))]
+        free_goals = [
+            g for g in to_visit if all(p in paths_count for p in goals._back_edges(g))
+        ]
         assert free_goals
         next_goal = free_goals[0]
         to_visit.remove(next_goal)
-        paths_count[next_goal] = max(1, sum(paths_count[p] for p in goals._back_edges(next_goal)))
+        paths_count[next_goal] = max(
+            1, sum(paths_count[p] for p in goals._back_edges(next_goal))
+        )
     return {
-        'goals': goals_count,
-        'edges': edges_count,
-        'paths': sum(paths_count.values()),
+        "goals": goals_count,
+        "edges": edges_count,
+        "paths": sum(paths_count.values()),
     }
