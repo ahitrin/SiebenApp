05d62c5132fb3d86f6e759cc0678bb6813eb0d53
Andrey Hitrin <andrey.hitrin@gmail.com>
2021-01-01 16:16:57 +0500

    Ignore few pylint warnings for now

--- siebenapp/app.py
+++ siebenapp/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
+# pylint: disable=too-many-locals,too-many-branches,too-many-statements
 import sys
 from argparse import ArgumentParser
 from os.path import dirname, join, realpath
