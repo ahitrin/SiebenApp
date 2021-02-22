39262b8cdea79b493792c57346056519fadb51e2
Andrey Hitrin <andrey.hitrin@gmail.com>
2021-02-22 16:46:52 +0500

    Switchable view: always keep current selection

--- siebenapp/switchable_view.py
+++ siebenapp/switchable_view.py
@@ -2,19 +2,7 @@ import collections
 from dataclasses import dataclass
 from typing import Dict, Any
 
-from siebenapp.domain import (
-    Command,
-    Graph,
-    Select,
-    HoldSelect,
-    Add,
-    ToggleClose,
-    Insert,
-    ToggleLink,
-    Delete,
-)
-
-TREE_MODIFIERS = [Add, Delete, Insert, ToggleClose, ToggleLink]
+from siebenapp.domain import Command, Graph
 
 
 @dataclass(frozen=True)
@@ -24,7 +12,7 @@ class ToggleSwitchableView(Command):
 
 class SwitchableView(Graph):
     """Non-persistent layer that allows to
-    show only switchable goals"""
+    show only switchable and/or selected goals"""
 
     def __init__(self, goaltree: Graph):
         super().__init__()
@@ -34,20 +22,8 @@ class SwitchableView(Graph):
     def accept(self, command: Command) -> None:
         if isinstance(command, ToggleSwitchableView):
             self._only_switchable = not self._only_switchable
-            self._fix_selection()
         else:
             self.goaltree.accept(command)
-            if command.__class__ in TREE_MODIFIERS:
-                self._fix_selection()
-
-    def _fix_selection(self):
-        if not self._only_switchable:
-            return
-        ids = [k for k, v in self.goaltree.q("switchable").items() if v["switchable"]]
-        if ids and self.goaltree.settings("selection") not in ids:
-            self.goaltree.accept(Select(min(ids)))
-        if ids and self.goaltree.settings("previous_selection") not in ids:
-            self.goaltree.accept(HoldSelect())
 
     def events(self) -> collections.deque:
         return self.goaltree.events()
@@ -61,9 +37,19 @@ class SwitchableView(Graph):
         skip_switchable = "switchable" not in keys
         if skip_switchable:
             keys = ",".join([keys, "switchable"])
+        pass_through = [
+            k
+            for k in [
+                self.goaltree.settings("selection"),
+                self.goaltree.settings("previous_selection"),
+            ]
+            if k is not None
+        ]
         goals = self.goaltree.q(keys)
         if self._only_switchable:
-            goals = {k: v for k, v in goals.items() if v["switchable"]}
+            goals = {
+                k: v for k, v in goals.items() if v["switchable"] or k in pass_through
+            }
             for v in goals.values():
                 if "edge" in v:
                     v["edge"] = []
--- siebenapp/tests/test_switchable_view.py
+++ siebenapp/tests/test_switchable_view.py
@@ -1,52 +1,27 @@
-from siebenapp.domain import Insert, Add, Select, ToggleClose
-from siebenapp.switchable_view import ToggleSwitchableView, SwitchableView
+from siebenapp.domain import Add, Select, HoldSelect
 from siebenapp.goaltree import Goals
-from siebenapp.open_view import OpenView
+from siebenapp.switchable_view import ToggleSwitchableView, SwitchableView
 from siebenapp.tests.dsl import build_goaltree, open_, selected, previous
 from siebenapp.zoom import ToggleZoom, Zoom
 
 
-def test_goaltree_selection_may_be_changed_in_switchable_view():
+def test_toggle_hide_non_switchable_goals():
     g = build_goaltree(
-        open_(1, "Root", [2, 3], select=selected),
-        open_(2, "Switchable 1"),
-        open_(3, "Switchable 2"),
-    )
-    e = SwitchableView(g)
-    assert e.q(keys="name,switchable,select") == {
-        1: {"name": "Root", "switchable": False, "select": "select"},
-        2: {"name": "Switchable 1", "switchable": True, "select": None},
-        3: {"name": "Switchable 2", "switchable": True, "select": None},
-    }
-    e.accept(ToggleSwitchableView())
-    assert g.events()[-2] == ("select", 2)
-    assert g.events()[-1] == ("hold_select", 2)
-    assert e.q(keys="name,switchable,select") == {
-        2: {"name": "Switchable 1", "switchable": True, "select": "select"},
-        3: {"name": "Switchable 2", "switchable": True, "select": None},
-    }
-
-
-def test_goaltree_previous_selection_may_be_changed_in_switchable_view():
-    g = build_goaltree(
-        open_(1, "Root", [2, 3], select=previous),
+        open_(1, "Root", [2, 3]),
         open_(2, "Switchable 1", select=selected),
         open_(3, "Switchable 2"),
     )
     e = SwitchableView(g)
     assert e.q(keys="name,switchable,select") == {
-        1: {"name": "Root", "switchable": False, "select": "prev"},
+        1: {"name": "Root", "switchable": False, "select": None},
         2: {"name": "Switchable 1", "switchable": True, "select": "select"},
         3: {"name": "Switchable 2", "switchable": True, "select": None},
     }
     e.accept(ToggleSwitchableView())
-    assert g.events()[-1] == ("hold_select", 2)
     assert e.q(keys="name,switchable,select") == {
         2: {"name": "Switchable 1", "switchable": True, "select": "select"},
         3: {"name": "Switchable 2", "switchable": True, "select": None},
     }
-    e.accept(Insert("Illegal goal"))
-    # New goal must not be inserted because previous selection is reset after the view switching
     e.accept(ToggleSwitchableView())
     assert e.q(keys="name,switchable,select") == {
         1: {"name": "Root", "switchable": False, "select": None},
@@ -55,35 +30,35 @@ def test_goaltree_previous_selection_may_be_changed_in_switchable_view():
     }
 
 
-def test_switchable_view_may_be_empty_when_underlying_layer_is_empty():
-    e = SwitchableView(OpenView(Goals("closed")))
-    e.accept_all(ToggleClose(), ToggleSwitchableView())
-    assert e.q() == {}
+def test_do_not_hide_unswitchable_goals_when_they_have_selection():
+    v = SwitchableView(
+        build_goaltree(
+            open_(1, "Selected", [2], select=selected),
+            open_(2, "Prev-selected", [3], select=previous),
+            open_(3, "Switchable"),
+        )
+    )
+    v.accept_all(ToggleSwitchableView())
+    assert v.q("name,switchable,select") == {
+        1: {"name": "Selected", "switchable": False, "select": "select"},
+        2: {"name": "Prev-selected", "switchable": False, "select": "prev"},
+        3: {"name": "Switchable", "switchable": True, "select": None},
+    }
 
 
-def test_simple_switchable_enumeration_workflow():
+def test_non_switchable_goals_disappear_on_selection_change():
     e = SwitchableView(Goals("root"))
     e.accept_all(Add("1"), Add("2"), Select(2), ToggleSwitchableView(), Select(2))
-    assert e.q() == {2: {"name": "1"}, 3: {"name": "2"}}
-
-
-def test_change_selection_on_goal_add():
-    v = SwitchableView(Goals("Root"))
-    v.accept_all(ToggleSwitchableView(), Add("Must be selected"))
-    assert v.q("name,select") == {2: {"name": "Must be selected", "select": "select"}}
-
-
-def test_change_selection_on_goal_closing():
-    g = build_goaltree(
-        open_(1, "Root", [2]),
-        open_(2, "Subroot", [3]),
-        open_(3, "Closing", select=selected),
-    )
-    v = SwitchableView(OpenView(g))
-    v.accept(ToggleSwitchableView())
-    assert v.q("name,select") == {3: {"name": "Closing", "select": "select"}}
-    v.accept(ToggleClose())
-    assert v.q("name,select") == {2: {"name": "Subroot", "select": "select"}}
+    assert e.q("name,switchable,select") == {
+        1: {"name": "root", "switchable": False, "select": "prev"},
+        2: {"name": "1", "switchable": True, "select": "select"},
+        3: {"name": "2", "switchable": True, "select": None},
+    }
+    e.accept(HoldSelect())
+    assert e.q("name,switchable,select") == {
+        2: {"name": "1", "switchable": True, "select": "select"},
+        3: {"name": "2", "switchable": True, "select": None},
+    }
 
 
 def test_how_should_we_deal_with_zooming():
@@ -94,10 +69,16 @@ def test_how_should_we_deal_with_zooming():
     )
     v = SwitchableView(Zoom(g))
     v.accept_all(ToggleZoom(), ToggleSwitchableView())
-    assert v.q("name,select") == {3: {"name": "Ex-top", "select": "select"}}
+    assert v.q("name,select") == {
+        2: {"name": "Zoomed", "select": "select"},
+        3: {"name": "Ex-top", "select": None},
+    }
     v.accept(Add("Unexpectedly hidden"))
-    # I definitely don't like this behavior. Seems that we should show something here
-    assert v.q("name,select") == {}
+    assert v.q("name,select") == {
+        2: {"name": "Zoomed", "select": "select"},
+        3: {"name": "Ex-top", "select": None},
+        4: {"name": "Unexpectedly hidden", "select": None},
+    }
 
 
 def test_filter_switchable_setting_is_not_set_by_default():
