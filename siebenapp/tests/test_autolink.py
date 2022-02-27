a03ce35aaf3ba666f0df2d5fd4108f4515ce4c55
Andrey Hitrin <andrey.hitrin@gmail.com>
2022-02-27 21:21:35 +0500

    Design autolink test cases #181

---
+++ siebenapp/tests/test_autolink.py
@@ -0,0 +1,15 @@
+"""
+Autolink layer test cases TBD
+
+* add autolink -> show a new pseudogoal
+* remove autolink -> remove a pseudogoal
+* matching goal already exists, add autolink -> show new pseudogoal, do not link
+* add autolink, add non-matching goal -> pass as is
+* add autolink, add matching goal -> make a link
+* add autolink, insert matching goal -> make a link
+* add autolink, rename existing goal -> make a link
+* add autolink, add matching subgoal -> do nothing
+* add autolink, rename existing subgoal -> do nothing
+* add 2 autolinks, add 1-matching goal -> make 1 link
+* add 2 autolinks, add 2-matching goal -> make 2 links
+"""
