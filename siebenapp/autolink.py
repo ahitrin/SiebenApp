5ded29a74a982ff894ae0777e58624e470a80c4e
Andrey Hitrin <andrey.hitrin@gmail.com>
2022-02-27 22:08:06 +0500

    Autolink: implement logic (1/14) #181

---
+++ siebenapp/autolink.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from typing import Dict, Any
+
+from siebenapp.domain import Graph, Command, with_key, EdgeType
+
+
+@dataclass(frozen=True)
+class ToggleAutoLink(Command):
+    keyword: str
+
+
+class AutoLink(Graph):
+    def __init__(self, goals: Graph):
+        super(AutoLink, self).__init__(goals)
+        self.keywords: Dict[str, int] = {}
+
+    def accept_ToggleAutoLink(self, command: ToggleAutoLink):
+        self.keywords[command.keyword] = self.settings("selection")
+
+    @with_key("edge")
+    def q(self, keys: str = "name") -> Dict[int, Any]:
+        goals = self.goaltree.q(keys)
+        new_goals: Dict[int, Any] = dict(goals)
+        for keyword, goal_id in self.keywords.items():
+            pseudo_id = -(goal_id + 10)
+            for real_goal, attrs in goals.items():
+                real_edges = attrs.pop("edge")
+                new_edges = [
+                    e if e[0] != goal_id else (pseudo_id, e[1]) for e in real_edges
+                ]
+                attrs["edge"] = new_edges
+                new_goals[real_goal] = attrs
+            pseudo_goal: Dict[str, Any] = {"edge": [(goal_id, EdgeType.PARENT)]}
+            if "name" in keys:
+                pseudo_goal["name"] = f"Autolink: '{keyword}'"
+            if "open" in keys:
+                pseudo_goal["open"] = True
+            if "select" in keys:
+                pseudo_goal["select"] = None
+            if "switchable" in keys:
+                pseudo_goal["switchable"] = False
+            new_goals[pseudo_id] = pseudo_goal
+        return new_goals
--- siebenapp/tests/test_autolink.py
+++ siebenapp/tests/test_autolink.py
@@ -1,8 +1,11 @@
 """
 Autolink layer test cases TBD
 
-* add autolink -> show a new pseudogoal
-* remove autolink -> remove a pseudogoal
+* ✓ add autolink -> show a new pseudogoal
+* add autolink1, add autolink 2 -> replace autolink1 with autolink2
+* add autolink, add empty autolink -> remove a pseudogoal
+* add autolink on closed goal -> do not add, show error
+* add autolink, close goal -> remove autolink
 * matching goal already exists, add autolink -> show new pseudogoal, do not link
 * add autolink, add non-matching goal -> pass as is
 * add autolink, add matching goal -> make a link
@@ -13,3 +16,29 @@ Autolink layer test cases TBD
 * add 2 autolinks, add 1-matching goal -> make 1 link
 * add 2 autolinks, add 2-matching goal -> make 2 links
 """
+from siebenapp.autolink import AutoLink, ToggleAutoLink
+from siebenapp.domain import EdgeType
+from siebenapp.tests.dsl import build_goaltree, open_, selected
+
+
+def test_show_new_pseudogoal_on_autolink_event():
+    goals = build_goaltree(
+        open_(1, "Root", [2]), open_(2, "Should be autolinked", select=selected)
+    )
+    al = AutoLink(goals)
+    assert al.q("name,edge,select") == {
+        1: {"name": "Root", "edge": [(2, EdgeType.PARENT)], "select": None},
+        2: {"name": "Should be autolinked", "edge": [], "select": "select"},
+    }
+    al.accept(ToggleAutoLink("hello"))
+    assert al.q("edge") == {
+        1: {"edge": [(-12, EdgeType.PARENT)]},
+        -12: {"edge": [(2, EdgeType.PARENT)]},
+        2: {"edge": []},
+    }
+    assert al.q("name,open,select,switchable")[-12] == {
+        "name": "Autolink: 'hello'",
+        "open": True,
+        "select": None,
+        "switchable": False,
+    }
