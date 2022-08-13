bfc9c82d07a991ddc4d77ad94e346943aad78d30
Andrey Hitrin <andrey.hitrin@gmail.com>
2022-08-13 20:52:38 +0500

    Ignore keys in OpenView.q() #329

--- siebenapp/open_view.py
+++ siebenapp/open_view.py
@@ -30,36 +30,37 @@ class OpenView(Graph):
         if not origin.settings("filter_open"):
             self.accept(ToggleOpenView())
 
-    def _is_visible(self, goal_id: int, attrs) -> bool:
+    def _is_visible(self, goal_id: GoalId, attrs) -> bool:
         if self._open:
             # Here we know something about other layers (Zoom). I do not like it
             return attrs["open"] or goal_id in self.selections() or goal_id in {1, -1}
         return True
 
     @with_key("open")
+    @with_key("edge")
     def q(self, keys: str = "name") -> RenderResult:
-        goals = self.goaltree.q(keys).slice(keys)
-        result: Dict[int, Any] = {
+        goals = self.goaltree.q(keys).graph
+        result: Dict[GoalId, Any] = {
             k: {} for k, v in goals.items() if self._is_visible(k, v)
         }
         # goals without parents
-        not_linked: Set[int] = set(g for g in result.keys() if g > 1)
+        not_linked: Set[GoalId] = set(
+            g for g in result.keys() if isinstance(g, int) and g > 1
+        )
         for goal_id in result:
             val = goals[goal_id]
             result[goal_id] = {k: v for k, v in val.items() if k != "edge"}
-            if "edge" in keys:
-                filtered_edges: List[Tuple[int, EdgeType]] = []
-                for edge in val["edge"]:
-                    if edge[0] in result:
-                        filtered_edges.append(edge)
-                        if edge[0] > 1:
-                            not_linked.discard(edge[0])
-                result[goal_id]["edge"] = filtered_edges
-        if "edge" in keys:
-            root_goals: Set[int] = set(result.keys()).intersection({1, -1})
-            if root_goals:
-                root_goal: int = root_goals.pop()
-                for missing_goal in not_linked:
-                    result[root_goal]["edge"].append((missing_goal, EdgeType.BLOCKER))
+            filtered_edges: List[Tuple[int, EdgeType]] = []
+            for edge in val["edge"]:
+                if edge[0] in result:
+                    filtered_edges.append(edge)
+                    if edge[0] > 1:
+                        not_linked.discard(edge[0])
+            result[goal_id]["edge"] = filtered_edges
+        root_goals: Set[GoalId] = set(result.keys()).intersection({1, -1})
+        if root_goals:
+            root_goal: GoalId = root_goals.pop()
+            for missing_goal in not_linked:
+                result[root_goal]["edge"].append((missing_goal, EdgeType.BLOCKER))
         repacked: Dict[GoalId, Any] = {k: v for k, v in result.items()}
         return RenderResult(repacked, {})
