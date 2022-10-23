5860cfd62a9a902db066f4dee8cb74865fdfae2d
Andrey Hitrin <andrey.hitrin@gmail.com>
2022-10-23 13:12:34 +0400

    Fix: copy previous row attributes in Zoom #344

--- siebenapp/zoom.py
+++ siebenapp/zoom.py
@@ -68,7 +68,11 @@ class Zoom(Graph):
                 r.is_open,
                 r.is_switchable,
                 [e for e in r.edges if e[0] in visible_goals.union(selected_goals)],
-                {"Zoom": "root"} if r.goal_id == self.zoom_root[-1] else {},
+                {
+                    # We could use `r.attrs | (...)` in Python 3.9+
+                    **r.attrs,
+                    **({"Zoom": "root"} if r.goal_id == self.zoom_root[-1] else {}),
+                },
             )
             for r in rows
             if r.goal_id in visible_goals.union(selected_goals)
