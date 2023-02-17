4548eb0ac2f042f3e192fbac9cf78baaf3d12758
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-02-17 11:15:40 +0500

    cli: split fmt() into smaller functions

--- siebenapp/cli.py
+++ siebenapp/cli.py
@@ -59,29 +59,41 @@ def update_message(message: str = "") -> None:
 
 
 def fmt(render_result: RenderResult, row: RenderRow, id_width: int) -> str:
-    show_id: str = " " * id_width if -10 <= int(row.goal_id) < 0 else str(row.goal_id)
-    op: str = " " if row.is_open else "x"
-    status: str = f"[{op}]" if row.is_switchable else f" {op} "
-    selection: str = " "
-    if render_result.select[0] == row.goal_id:
-        selection = ">"
-    elif render_result.select[1] == row.goal_id:
-        selection = "_"
-    children: str = ""
-    if row.edges:
+    def show_id() -> str:
+        return " " * id_width if -10 <= int(row.goal_id) < 0 else str(row.goal_id)
+
+    def status() -> str:
+        op: str = " " if row.is_open else "x"
+        return f"[{op}]" if row.is_switchable else f" {op} "
+
+    def selection() -> str:
+        if render_result.select[0] == row.goal_id:
+            return ">"
+        elif render_result.select[1] == row.goal_id:
+            return "_"
+        return " "
+
+    def children() -> str:
+        if not row.edges:
+            return ""
         child_list: str = ",".join(
             str(e[0]) for e in row.edges if e[1] == EdgeType.PARENT
         )
         blocker_list: str = ",".join(
             str(e[0]) for e in row.edges if e[1] == EdgeType.BLOCKER
         )
-        children = f" [{child_list} / {blocker_list}]"
-    attributes: str = (
-        f" [{','.join(f'{k}: {row.attrs[k]}' for k in sorted(row.attrs))}]"
-        if row.attrs
-        else ""
+        return f" [{child_list} / {blocker_list}]"
+
+    def attributes() -> str:
+        return (
+            f" [{','.join(f'{k}: {row.attrs[k]}' for k in sorted(row.attrs))}]"
+            if row.attrs
+            else ""
+        )
+
+    return "".join(
+        [show_id(), status(), selection(), row.name, children(), attributes()]
     )
-    return f"{show_id}{status}{selection}{row.name}{children}{attributes}"
 
 
 def build_actions(command: str) -> List[Command]:
