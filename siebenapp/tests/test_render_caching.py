9227cd28ca6223d3372167e017451a9ef488d1f2
Andrey Hitrin <andrey.hitrin@gmail.com>
2023-04-08 20:20:55 +0500

    Simple preliminary tests on GoalsHolder #371

---
+++ siebenapp/tests/test_render_caching.py
@@ -0,0 +1,63 @@
+import pytest
+
+from siebenapp.autolink import ToggleAutoLink
+from siebenapp.domain import Add, Rename, ToggleClose, Delete, Insert
+from siebenapp.filter_view import FilterBy
+from siebenapp.layers import all_layers
+from siebenapp.open_view import ToggleOpenView
+from siebenapp.progress_view import ToggleProgress
+from siebenapp.render import GoalsHolder
+from siebenapp.switchable_view import ToggleSwitchableView
+from siebenapp.tests.dsl import build_goaltree, open_, selected, previous, clos_
+from siebenapp.zoom import ToggleZoom
+
+WIDTH = 3
+
+
+@pytest.fixture
+def sample_tree():
+    return all_layers(
+        build_goaltree(
+            open_(1, "Root", [2, 3, 4]),
+            open_(2, "Selected", select=selected),
+            open_(3, "Previous", [5], blockers=[4], select=previous),
+            open_(4, "Blocked", blockers=[6]),
+            clos_(5, "Closed intermediate", [6]),
+            clos_(6, "Closed top"),
+        )
+    )
+
+
+@pytest.fixture
+def sample_holder(sample_tree):
+    return GoalsHolder(sample_tree, ":memory:", True)
+
+
+def test_no_diff_on_start(sample_holder):
+    holder = sample_holder
+    result = holder.render(WIDTH)
+    assert result[1] == []
+
+
+@pytest.mark.parametrize(
+    "event",
+    [
+        Add("New"),
+        Insert("Middle"),
+        Rename("New name"),
+        ToggleClose(),
+        Delete(),
+        FilterBy("anything"),
+        ToggleAutoLink("keyword"),
+        ToggleProgress(),
+        ToggleZoom(),
+        ToggleSwitchableView(),
+        ToggleOpenView(),
+    ],
+)
+def test_no_diff_when_goaltree_changes(sample_holder, event):
+    holder = sample_holder
+    holder.render(WIDTH)  # Prepare cached result
+    holder.accept(event)
+    result = holder.render(WIDTH)
+    assert result[1] == []
