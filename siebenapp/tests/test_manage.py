36c72406d00cf0ec1be9aff3847f2150b7dadcab
Andrey Hitrin <andrey.hitrin@gmail.com>
2024-09-15 23:37:52 +0500

    test_manage: migrate to the new test DSL #538

--- siebenapp/tests/test_manage.py
+++ siebenapp/tests/test_manage.py
@@ -8,7 +8,7 @@ from siebenapp.enumeration import Enumeration
 from siebenapp.layers import all_layers, persistent_layers
 from siebenapp.manage import main, dot_export, extract_subtree
 from siebenapp.system import save, load
-from siebenapp.tests.dsl import build_goaltree, open_, clos_, selected, previous
+from siebenapp.tests.dsl import build_goaltree, open_, clos_
 from siebenapp.tests.test_cli import DummyIO, verify_file
 from siebenapp.zoom import ToggleZoom
 
@@ -41,15 +41,16 @@ def complex_goaltree_file():
         open_(1, "Root goal of a complex goal tree", [2, 3, 4, 5, 6, 12], [7, 8]),
         clos_(2, "Closed", blockers=[7], relations=[12]),
         open_(3, "Simply 3", blockers=[5, 8], relations=[4, 12]),
-        open_(4, "Also 4", blockers=[5, 6, 7, 8], select=selected),
+        open_(4, "Also 4", blockers=[5, 6, 7, 8]),
         open_(5, "Now 5", blockers=[6]),
         clos_(6, "Same 6", blockers=[7]),
-        clos_(7, "Lucky 7", [8], select=previous),
+        clos_(7, "Lucky 7", [8]),
         clos_(8, "Finally 8", [9]),
         clos_(9, "Nested 9", [10]),
         clos_(10, "More nested 10", [11]),
         clos_(11, "Even more nested 11"),
         open_(12, "Informational 12"),
+        select=(4, 7),
     )
     file_name = NamedTemporaryFile().name
     save(all_layers(g), file_name)
@@ -105,11 +106,12 @@ def test_dot_export() -> None:
             "This is closed goal with no children or blockers. "
             "It also has a long name that must be compacted",
         ),
-        open_(3, 'I have some "special" symbols', [6, 7], select=selected),
+        open_(3, 'I have some "special" symbols', [6, 7]),
         clos_(4, ""),
         open_(5, "Many blockerz", blockers=[2, 4, 6, 7]),
         clos_(6, "!@#$%^&*()\\/,.?"),
-        open_(7, ";:[{}]<>", select=previous),
+        open_(7, ";:[{}]<>"),
+        select=(3, 7),
     )
     verify_file(dot_export(goals), ".dot")
 
@@ -157,7 +159,7 @@ def extract_source():
     return Enumeration(
         all_layers(
             build_goaltree(
-                open_(1, "Root NX", [2, 3], select=selected),
+                open_(1, "Root NX", [2, 3]),
                 open_(2, "Extract root", [4, 5], blockers=[3]),
                 open_(3, "External blocker NX", [7]),
                 open_(4, "Subgoal", blockers=[5]),
@@ -165,10 +167,10 @@ def extract_source():
                     5,
                     "Selected subgoal (selection will be lost)",
                     [6],
-                    select=previous,
                 ),
                 clos_(6, "Closed subgoal", blockers=[7]),
                 clos_(7, "Another external blocker NX"),
+                select=(1, 5),
             )
         )
     )
@@ -178,10 +180,11 @@ def extract_source():
 def extract_target():
     return persistent_layers(
         build_goaltree(
-            open_(1, "Extract root", [2, 3], select=selected),
+            open_(1, "Extract root", [2, 3]),
             open_(2, "Subgoal", blockers=[3]),
             open_(3, "Selected subgoal (selection will be lost)", [4]),
             clos_(4, "Closed subgoal"),
+            select=(1, 1),
         )
     )
 
@@ -207,9 +210,10 @@ def test_extract_misordered() -> None:
     source = Enumeration(
         all_layers(
             build_goaltree(
-                open_(1, "Global root", [3], select=selected),
+                open_(1, "Global root", [3]),
                 open_(2, "Top"),
                 open_(3, "Extraction root", [2]),
+                select=(1, 1),
             )
         )
     )
