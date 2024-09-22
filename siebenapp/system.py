84355e1b956296e300a610fee3f89da4de951ffb
Andrey Hitrin <andrey.hitrin@gmail.com>
2024-09-22 10:40:01 +0500

    system: reuse new get_roots() to simplify save_connection() #538

--- siebenapp/system.py
+++ siebenapp/system.py
@@ -125,13 +125,9 @@ def save_connection(goals: Graph, connection) -> None:
     run_migrations(connection)
     root_goals: Goals = get_root(goals, Goals)
     goals_export, edges_export, select_export = Goals.export(root_goals)
-    zoom_goals = goals
-    while not isinstance(zoom_goals, Zoom):
-        zoom_goals = zoom_goals.goaltree
+    zoom_goals = get_root(goals, Zoom)
     zoom_export = Zoom.export(zoom_goals)
-    autolink_goals = goals
-    while not isinstance(autolink_goals, AutoLink):
-        autolink_goals = autolink_goals.goaltree
+    autolink_goals = get_root(goals, AutoLink)
     autolink_export = AutoLink.export(autolink_goals)
     cur = connection.cursor()
     cur.executemany("insert into goals values (?,?,?)", goals_export)
